a6b5ac6dd9b08fde9a29ce47a795e0f9734c2322
mendy hershkop <hers.mm@gmail.com>
2025-11-06 10:03:50 +0200

    create monster class

---
+++ core/monster.py
@@ -0,0 +1,11 @@
+from core.fighters import Fighters
+
+
+class Monster(Fighters):
+    def __init__(self, name, hp, speed, power, armor_rating):
+        super().__init__(name,hp,speed,power,armor_rating)
+            self.weapons=["סכין","חרב","גרזן"]
+   def speak():
+       pass
+   def attack():
+       pass
